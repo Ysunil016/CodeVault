f38f5fd39449ed95bd737ec282e7fd08da6ea0bd
Sunil <sunil016@yahoo.com>
2020-02-06 23:15:19 +0530

    Leet Code Problems

--- LeetCode/src/Array/RemoveDuplicates.java
+++ LeetCode/src/Array/RemoveDuplicates.java
@@ -3,7 +3,6 @@ package Array;
 public class RemoveDuplicates {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		int[] arr = { 0, 0, 0, 2, 2, 3, 4, 5 };
 		int result = removeDups(arr);
 		System.out.println("Result is " + result);
@@ -12,9 +11,6 @@ public class RemoveDuplicates {
 		}
 	}
 
-	/*
-	 * Taking 2 Pointers One at 1 and Other Traversing through Array
-	 */
 
 	static int removeDups(int[] arr) {
 		if (arr.length == 0)
@@ -23,13 +19,15 @@ public class RemoveDuplicates {
 		int first = 0;
 		int second = 1;
 
-		int result = 0;
+		int result = 1;
 		for (int i = 1; i < arr.length; i++) {
 			if (arr[i] == arr[first]) {
-				second = i;
+				second++;
 			} else {
 				arr[first + 1] = arr[second];
+				second++;
 				first++;
+				result++;
 			}
 		}
 		return result;
