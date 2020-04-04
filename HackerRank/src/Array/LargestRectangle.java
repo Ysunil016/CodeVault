ceeaa7415cb8661d4f95fd11c91c87ac2e86d3dd
Sunil <sunil016@yahoo.com>
2020-04-04 17:03:14 +0530

    HackerRank Largest Rectangle

---
+++ HackerRank/src/Array/LargestRectangle.java
@@ -0,0 +1,32 @@
+package Array;
+
+public class LargestRectangle {
+
+	public static void main(String[] args) {
+		int[] arr = { 3, 5, 3, 2, 4, 2, 6, 3, 3, 2, 5 };
+		System.out.println(getLargestRectangle(arr));
+	}
+
+	// Finding Max Element for an Building Given an Array of Heights.
+	// Condition is Rectangle Area will be Counters in Increments i -> i+1 -> i+2
+	// And Also we have to find the max of all the concurrent area found by
+	// comparing each and every heights form the building.
+	private static long getLargestRectangle(int[] arr) {
+		int result = Integer.MIN_VALUE;
+		for (int i = 0; i < arr.length; i++) {
+			int areaOfRectangle = arr[i];
+			int initHeight = arr[i];
+			for (int j = i + 1; j < arr.length; j++) {
+				if (initHeight <= arr[j])
+					areaOfRectangle += arr[i];
+				else
+					break;
+			}
+			// Checking for Every Height, is it more from our last stored??
+			result = Math.max(result, areaOfRectangle);
+		}
+
+		return result;
+	}
+
+}
