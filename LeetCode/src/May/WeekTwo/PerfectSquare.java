3e4d209ee33f79a97620e3c4dd4220e171f2ec43
Sunil <sunil016@yahoo.com>
2020-05-09 15:41:09 +0530

    Leet Code 8 Perfect Square Optamized

---
+++ LeetCode/src/May/WeekTwo/PerfectSquare.java
@@ -0,0 +1,44 @@
+package May.WeekTwo;
+
+import java.math.BigInteger;
+
+public class PerfectSquare {
+
+	public static void main(String[] args) {
+		System.out.println(isPerfectSqr(2147395600));
+	}
+
+	public static boolean isPerfectSquare(int num) {
+		int Counter = 1;
+		int Product = 1;
+		while (Product < num) {
+			Product = Counter * Counter;
+			Counter++;
+		}
+		if (Product == num) {
+			return true;
+		}
+		return false;
+	}
+
+	public static boolean isPerfectSqr(int num) {
+		if (num == 1)
+			return true;
+
+		int low = 1;
+		int high = num / 2;
+		while (low <= high) {
+			int mid = low + (high - low) / 2;
+			if (mid == num / mid && num % mid == 0) {
+				return true;
+			} else if (mid < num / mid) {
+				low = mid + 1;
+			} else {
+				high = mid - 1;
+			}
+		}
+		return false;
+
+	}
+
+}
