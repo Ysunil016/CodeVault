92c7d7a7457b3ce25c9f540be073db79c7a6da6f
Sunil <sunil016@yahoo.com>
2020-05-16 14:03:31 +0530

    LeetCode Odd Even Linked List - May Coding Day 16

---
+++ LeetCode/src/May/WeekThree/OddEvenLinkedList.java
@@ -0,0 +1,63 @@
+package May.WeekThree;
+
+public class OddEvenLinkedList {
+
+	static class Node {
+		int data;
+		Node next = null;
+
+		Node(int data) {
+			this.data = data;
+		}
+	}
+
+	public static void main(String[] args) {
+		Node head = new Node(1);
+		head.next = new Node(2);
+		head.next.next = new Node(3);
+		head.next.next.next = new Node(4);
+		head.next.next.next.next = new Node(5);
+//		head.next.next.next.next.next = new Node(4);
+//		head.next.next.next.next.next.next = new Node(7);
+
+//		displayNode(head);
+		jumbleList(head);
+
+	}
+
+	static void jumbleList(Node head) {
+		if (head == null)
+			return;
+		if (head.next == null)
+			return;
+		Node currentNode = head;
+		Node odd = new Node(-1);
+		Node even = new Node(-1);
+		Node oddC = odd;
+		Node evenC = even;
+
+		while (currentNode != null) {
+			oddC.next = currentNode;
+			oddC = oddC.next;
+			if (currentNode.next != null) {
+				evenC.next = currentNode.next;
+				evenC = evenC.next;
+			} else {
+				evenC.next = null;		
+				break;
+			}
+			currentNode = currentNode.next.next;
+		}
+
+		oddC.next = even.next;
+		displayNode(odd.next);
+	}
+
+	static void displayNode(Node root) {
+		while (root != null) {
+			System.out.print(root.data + " ");
+			root = root.next;
+		}
+	}
+
+}
