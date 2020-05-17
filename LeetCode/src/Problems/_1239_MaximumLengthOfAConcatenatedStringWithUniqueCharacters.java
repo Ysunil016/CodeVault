d81a420340df95632d8c34018bbc4b8f748f29d3
Sunil <sunil016@yahoo.com>
2020-05-18 00:34:41 +0530

    Problem 1239, Max Length Of Concatenated String with Unique Character

---
+++ LeetCode/src/Problems/_1239_MaximumLengthOfAConcatenatedStringWithUniqueCharacters.java
@@ -0,0 +1,52 @@
+package Problems;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class _1239_MaximumLengthOfAConcatenatedStringWithUniqueCharacters {
+
+	public static void main(String[] args) {
+		Solution sol = new Solution();
+		List<String> arr = new ArrayList<String>() {
+			{
+				add("cha");
+				add("r");
+				add("act");
+				add("ers");
+			}
+		};
+		System.out.println(sol.maxLength(arr));
+	}
+
+}
+
+class Solution {
+	public int maxLength(List<String> arr) {
+		int[] result = new int[1]; // Result through Recursion
+		findUnique(arr, 0, "", result);
+		return result[0];
+	}
+
+	private void findUnique(List<String> arr, int index, String current, int[] result) {
+		if (index == arr.size() && charCount(current) > result[0]) {
+			result[0] = current.length();
+		}
+		if (index == arr.size()) {
+			return;
+		}
+		findUnique(arr, index + 1, current, result);
+		findUnique(arr, index + 1, current + arr.get(index), result);
+
+	}
+
+	private int charCount(String str) {
+		int[] counts = new int[26];
+		for (char c : str.toCharArray()) {
+			if (counts[c - 'a']++ > 0) {
+				return -1;
+			}
+		}
+		return str.length();
+	}
+
+}
