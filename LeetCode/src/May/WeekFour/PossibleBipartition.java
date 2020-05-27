3661ab89c23a1bbde86ab54194123a6bb03e2ebb
Sunil <sunil016@yahoo.com>
2020-05-27 20:18:57 +0530

    Leetcode May Challenge BiPartite Graph

---
+++ LeetCode/src/May/WeekFour/PossibleBipartition.java
@@ -0,0 +1,52 @@
+package May.WeekFour;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.LinkedList;
+
+public class PossibleBipartition {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[][] arr = { { 1, 2 }, { 1, 3 }, { 2, 4 } };
+		System.out.println(possibleBipartition(4, arr));
+	}
+
+	public static boolean possibleBipartition(int N, int[][] dislikes) {
+
+		int[] visited = new int[N];
+		Arrays.fill(visited, -1);
+
+		ArrayList<Integer>[] adjArray = new ArrayList[N];
+		for (int i = 0; i < N; i++) {
+			adjArray[i] = new ArrayList<Integer>();
+		}
+		// Populating ADG List
+		for (int i = 0; i < dislikes.length; i++) {
+			adjArray[dislikes[i][0] - 1].add(dislikes[i][1] - 1);
+			adjArray[dislikes[i][1] - 1].add(dislikes[i][0] - 1);
+		}
+
+		for (int i = 0; i < N; i++) {
+			if (visited[i] == -1 && !dfs(adjArray, visited, i, 0))
+				return false;
+		}
+
+		return true;
+	}
+
+	static boolean dfs(ArrayList<Integer>[] adjArray, int[] visited, int index, int grp) {
+		if (visited[index] == -1) {
+			visited[index] = grp;
+		} else
+			return visited[index] == grp;
+
+		for (int X : adjArray[index]) {
+			if (!dfs(adjArray, visited, X, 1 - grp))
+				return false;
+		}
+		return true;
+	}
+
+}
