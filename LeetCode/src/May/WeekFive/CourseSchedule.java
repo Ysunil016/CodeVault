f7bbad0309a6f67a5059f2eda19cf82b5d5bf0f9
Sunil <sunil016@yahoo.com>
2020-05-30 14:26:40 +0530

    Leetcode May Coding Challenge

--- LeetCode/src/May/WeekFive/CourseSchedule.java
+++ LeetCode/src/May/WeekFive/CourseSchedule.java
@@ -13,16 +13,9 @@ public class CourseSchedule {
 	}
 
 	public static boolean canFinish(int numCourses, int[][] prerequisites) {
-
 		class Node {
-			int val;
 			int inBound = 0;
-			int outBound = 0;
 			ArrayList<Integer> neighbour = new ArrayList<Integer>();
-
-			public Node(int val) {
-				this.val = val;
-			}
 
 			public Node decrementInBound() {
 				this.inBound--;
@@ -33,31 +26,20 @@ public class CourseSchedule {
 				this.inBound++;
 				return this;
 			}
-
-			public Node incrementOutBound() {
-				this.outBound++;
-				return this;
-			}
-
 			public void addNeighbour(int val) {
 				ArrayList<Integer> neighbour = this.neighbour;
 				neighbour.add(val);
 				this.neighbour = neighbour;
 			}
 		}
-
-		// Making Graph First
 		Node[] nodeStore = new Node[numCourses];
-
 		for (int i = 0; i < numCourses; i++) {
-			nodeStore[i] = new Node(i);
+			nodeStore[i] = new Node();
 		}
-
 		for (int i = 0; i < prerequisites.length; i++) {
-			nodeStore[prerequisites[i][0]].incrementOutBound().addNeighbour(prerequisites[i][1]);
+			nodeStore[prerequisites[i][0]].addNeighbour(prerequisites[i][1]);
 			nodeStore[prerequisites[i][1]].incrementInBound();
 		}
-
 		Queue<Node> queue = new LinkedList<Node>();
 		int crossCount = 0;
 
@@ -67,7 +49,6 @@ public class CourseSchedule {
 				crossCount++;
 			}
 		}
-
 		while (!queue.isEmpty()) {
 			Node current = queue.poll();
 			for (int x : current.neighbour) {
@@ -83,30 +64,28 @@ public class CourseSchedule {
 }
 
 /*
-
-Course Schedule
-Solution
-There are a total of numCourses courses you have to take, labeled from 0 to numCourses-1.
-
-Some courses may have prerequisites, for example to take course 0 you have to first take course 1, which is expressed as a pair: [0,1]
-
-Given the total number of courses and a list of prerequisite pairs, is it possible for you to finish all courses?
-
- 
-
-Example 1:
-
-Input: numCourses = 2, prerequisites = [[1,0]]
-Output: true
-Explanation: There are a total of 2 courses to take. 
-             To take course 1 you should have finished course 0. So it is possible.
-Example 2:
-
-Input: numCourses = 2, prerequisites = [[1,0],[0,1]]
-Output: false
-Explanation: There are a total of 2 courses to take. 
-             To take course 1 you should have finished course 0, and to take course 0 you should
-             also have finished course 1. So it is impossible.
- 
-
-*/
+ * 
+ * Course Schedule Solution There are a total of numCourses courses you have to
+ * take, labeled from 0 to numCourses-1.
+ * 
+ * Some courses may have prerequisites, for example to take course 0 you have to
+ * first take course 1, which is expressed as a pair: [0,1]
+ * 
+ * Given the total number of courses and a list of prerequisite pairs, is it
+ * possible for you to finish all courses?
+ * 
+ * 
+ * 
+ * Example 1:
+ * 
+ * Input: numCourses = 2, prerequisites = [[1,0]] Output: true Explanation:
+ * There are a total of 2 courses to take. To take course 1 you should have
+ * finished course 0. So it is possible. Example 2:
+ * 
+ * Input: numCourses = 2, prerequisites = [[1,0],[0,1]] Output: false
+ * Explanation: There are a total of 2 courses to take. To take course 1 you
+ * should have finished course 0, and to take course 0 you should also have
+ * finished course 1. So it is impossible.
+ * 
+ * 
+ */
---
+++ LeetCode/src/May/WeekFive/KClosestCoord.java
@@ -0,0 +1,53 @@
+package May.WeekFive;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class KClosestCoord {
+
+	public static void main(String[] args) {
+		int[][] arr = new int[][] { { 3, 3 }, { 5, -1 }, { -2, 4 } };
+		for (int[] a : kClosest(arr, 1)) {
+			for (int x : a)
+				System.out.print(" " + x);
+		}
+	}
+
+	public static int[][] kClosest(int[][] points, int K) {
+
+		class Node implements Comparable<Node> {
+			int distance;
+			int[] coord = new int[2];
+
+			Node(int[] coord) {
+				this.distance = coord[0] * coord[0] + coord[1] * coord[1];
+				this.coord = coord;
+			}
+
+			@Override
+			public int compareTo(Node t) {
+				return (this.distance > t.distance) ? 1 : (this.distance < t.distance) ? -1 : 0;
+			}
+
+		}
+		ArrayList<Node> store = new ArrayList<Node>();
+		for (int i = 0; i < points.length; i++) {
+			store.add(new Node(points[i]));
+		}
+
+		Collections.sort(store);
+
+		int[][] result = new int[K][2];
+		int counter = 0;
+		for (Node x : store) {
+			result[counter] = (x.coord);
+			counter++;
+			if (counter >= K) {
+				break;
+			}
+		}
+
+		return result;
+	}
+
+}
