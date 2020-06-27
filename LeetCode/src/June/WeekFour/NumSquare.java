e8c3e403a68cefa9b76ad2b63cf1d815440d29f2
Sunil <sunil016@yahoo.com>
2020-06-27 22:51:03 +0530

    Pythian

--- LeetCode/src/June/WeekFour/NumSquare.java
+++ LeetCode/src/June/WeekFour/NumSquare.java
@@ -5,16 +5,24 @@ import java.util.Arrays;
 public class NumSquare {
 
 	public static void main(String[] args) {
-		System.out.println(make(10));
+		System.out.println(make(13));
 	}
 
 	public static int make(int n) {
 		int[] dp = new int[n + 1];
-		Arrays.fill(dp, n);
-		dp[0] = 0;
-		dp[1] = 1;
-
-		return 1;
+		Arrays.fill(dp, 0);
+		for (int i = 1; i <= n; i++) {
+			int minV = i;
+			int y = 1;
+			int sq = 1;
+			while (sq <= i) {
+				minV = Math.min(minV, 1 + dp[i - sq]);
+				y++;
+				sq = y * y;
+			}
+			dp[i] = minV;
+		}
+		return dp[n];
 	}
 
 }
