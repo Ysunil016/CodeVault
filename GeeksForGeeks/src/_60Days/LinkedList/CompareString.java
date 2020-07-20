053020ddc7e2dae64df2a09debe9467a04ed0db6
Sunil <sunil016@yahoo.com>
2020-07-20 15:26:13 +0530

    CompareString - Geeks for Geeks Day 1

--- GeeksForGeeks/src/_60Days/LinkedList/CompareString.java
+++ GeeksForGeeks/src/_60Days/LinkedList/CompareString.java
@@ -2,24 +2,52 @@ package _60Days.LinkedList;
 
 public class CompareString {
 
-	static class LLNode {
-		int val;
-		LLNode next;
+	static class ListNode {
+		char c;
+		ListNode next;
 
-		public LLNode(int val) {
-			this.val = val;
+		public ListNode(char c) {
+			this.c = c;
 		}
 	}
 
 	public static void main(String[] args) {
+		ListNode sOne = new ListNode('g');
+		sOne.next = new ListNode('e');
+		sOne.next.next = new ListNode('e');
+		sOne.next.next.next = new ListNode('k');
+		sOne.next.next.next.next = new ListNode('s');
+//		sOne.next.next.next.next.next = new ListNode('a');
 
+		ListNode tOne = new ListNode('g');
+		tOne.next = new ListNode('e');
+		tOne.next.next = new ListNode('e');
+		tOne.next.next.next = new ListNode('k');
+		tOne.next.next.next.next = new ListNode('s');
+//		tOne.next.next.next.next.next = new ListNode('b');
+
+		System.out.println(compareString(sOne, tOne));
 	}
-	
-	static int compareString(LLNode strOne, LLNode strTwo) {
-		
-		
-		
-		return 0;
+
+	static int compareString(ListNode strOne, ListNode strTwo) {
+		if ((strOne == null && strTwo != null) || (strOne != null && strTwo == null)) {
+			if (strOne != null) {
+				return 1;
+			} else {
+				return -1;
+			}
+		}
+		if (strOne == null && strTwo == null) {
+			return 0;
+		}
+		if (strOne.c != strTwo.c) {
+			if (strOne.c > strTwo.c) {
+				return 1;
+			} else {
+				return -1;
+			}
+		}
+		return compareString(strOne.next, strTwo.next);
 	}
 
 }
