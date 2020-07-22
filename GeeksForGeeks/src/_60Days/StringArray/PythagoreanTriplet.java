e3d594a47f2b2ab9292885207a39fa7977eb9d99
Sunil <sunil016@yahoo.com>
2020-07-23 05:16:24 +0530

    Pythogrean Triplate - Geeks for Geeks Day 3

--- GeeksForGeeks/src/_60Days/StringArray/PythagoreanTriplet.java
+++ GeeksForGeeks/src/_60Days/StringArray/PythagoreanTriplet.java
@@ -1,37 +1,29 @@
 package _60Days.StringArray;
 
 import java.util.Arrays;
+import java.util.HashSet;
 
 public class PythagoreanTriplet {
 
 	public static void main(String[] args) {
-		int[] Arr = { 3, 1, 4, 6, 5 };
+		int[] Arr = { 10, 4, 6, 12, 5, 13 };
+
 		System.out.println(hasArrayHasPythagorean(Arr));
 	}
 
 	private static boolean hasArrayHasPythagorean(int[] arr) {
 		// O(nlogn)
 		Arrays.sort(arr);
+
+		HashSet<Integer> hashStore = new HashSet<>();
+		for (int X : arr)
+			hashStore.add(X);
+
 		for (int I = 0; I < arr.length - 2; I++) {
-			int J = I + 1;
-			int K = arr.length - 1;
-
-			int A = arr[I] * arr[I];
-			while (J < K) {
-
-				int C = arr[K] * arr[K];
-
-				int ResultSum = A + arr[J] * arr[J];
-				if (ResultSum == C)
+			for (int J = I; J < arr.length; J++) {
+				if (hashStore.contains(I * I + J * J))
 					return true;
-
-				if (ResultSum <= C) {
-					K--;
-				} else {
-					J++;
-				}
 			}
-
 		}
 
 		return false;
