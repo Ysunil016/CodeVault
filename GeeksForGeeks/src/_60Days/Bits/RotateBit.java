ce2bafc34ae2b5b05f56e984d41718196a40c8df
Sunil <sunil016@yahoo.com>
2020-07-24 04:55:06 +0530

    Bit Rotate Left & Right- Geeks for Geeks Day 4

--- GeeksForGeeks/src/_60Days/Bits/RotateBit.java
+++ GeeksForGeeks/src/_60Days/Bits/RotateBit.java
@@ -3,10 +3,11 @@ package _60Days.Bits;
 public class RotateBit {
 
 	public static void main(String[] args) {
-		System.out.println(getRotatedNumber(10, 10));
+		System.out.println(getLeftRotatedNumber(16, 2));
+		System.out.println(getRightRotatedNumber(16,2));
 	}
 
-	static int getRotatedNumber(int number, int steps) {
+	static int getLeftRotatedNumber(int number, int steps) {
 		steps = steps % 32;
 		for (int i = 0; i < steps; i++) {
 			int lastBit = number & 0x80000000;
@@ -15,6 +16,19 @@ public class RotateBit {
 				number = number | 1;
 			} else
 				number = number << 1;
+		}
+		return number;
+	}
+
+	static int getRightRotatedNumber(int number, int steps) {
+		steps = steps % 32;
+		for (int i = 0; i < steps; i++) {
+			int lastBit = number & 1;
+			if (lastBit != 0) {
+				number = number >> 1;
+				number = number | 0x80000000;
+			} else
+				number = number >> 1;
 		}
 		return number;
 	}
