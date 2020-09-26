1be7ec8c6e506b1647ca8f55bbfda9efc414e3c1
YSunil016 <sunil016@yahoo.com>
2020-09-26 23:57:48 +0530

    Refactor

--- LeetCode/src/September/LongestNumber.java
+++ LeetCode/src/September/LongestNumber.java
@@ -13,9 +13,7 @@ public class LongestNumber {
         String[] s = new String[n];
         for(int i = 0; i < n; i++)
             s[i] = String.valueOf(nums[i]);
-
         Arrays.sort(s, (a, b) -> (b + a).compareTo(a + b));
-
         StringBuilder sb = new StringBuilder();
         for(String str : s)
             sb.append(str);
