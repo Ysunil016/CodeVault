ea0daa49100e9ad52dc892297c9bd8289e4e25c8
YSunil016 <sunil016@yahoo.com>
2020-10-03 23:13:50 +0530

    K-Diff Pairs in Array | Leetcode 3 Oct 2020

---
+++ LeetCode/src/October/K_Difference_Array.java
@@ -0,0 +1,25 @@
+package October;
+
+import java.util.HashMap;
+
+public class K_Difference_Array {
+
+    public static void main(String[] args) {
+        System.out.println(findPairs(new int[]{4,6,2,5,7,8},5));
+    }
+
+    public static int findPairs(int[] noms, int k) {
+        HashMap<Integer,Integer> hashMap = new HashMap<>();
+        for(int x:noms){
+            hashMap.put(x, hashMap.getOrDefault(x,0)+1);
+        }
+        int result = 0;
+        for(int key:hashMap.keySet()){
+            if((k > 0 && hashMap.containsKey(key+k)) || (k == 0 && hashMap.get(key+k) > 1)){
+                result++;
+            }
+        }
+        return result;
+    }
+
+}
