b2da58a62b78458cb47777d67f80c8c1bc417973
Sunil Yadav <sunil016@yahoo.com>
2020-10-14 17:51:02 +0530

    Leetcode #14 House Robber

---
+++ LeetCode/src/October/BankRobII.java
@@ -0,0 +1,26 @@
+package October;
+
+public class BankRobII {
+    public static void main(String[] args) {
+        System.out.println(rob(new int[]{1,2,3,1}));
+    }
+
+
+    public static int rob(int[] nums) {
+        if(nums.length==0) return nums[0];
+        return Math.max(robState(nums,0,nums.length-1),robState(nums,1, nums.length));
+    }
+
+    private static int robState(int[] nums,int start,int end){
+        int twoStepBack = 0;
+        int oneStepBack = 0;
+        int max = 0;
+
+        for(int i=start;i<end;i++){
+            max = Math.max(twoStepBack + nums[i],oneStepBack);
+            twoStepBack = oneStepBack;
+            oneStepBack = max;
+        }
+    return max;
+    }
+}
