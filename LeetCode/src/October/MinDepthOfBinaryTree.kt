d58d710a1f5e2d7fd4fad30509ad720f52a3ee53
Sunil Yadav <sunil016@yahoo.com>
2020-10-22 19:16:39 +0530

    Leetcode #22 Min Depth of Binary Tree

---
+++ LeetCode/src/October/MinDepthOfBinaryTree.kt
@@ -0,0 +1,27 @@
+package October
+
+import com.sun.source.tree.Tree
+
+fun main() {
+
+}
+
+class TreeNode(val value: Int){
+    var left:TreeNode? = null
+    var right:TreeNode? = null
+}
+
+fun minDepth(root: TreeNode?): Int {
+    if(root==null)
+        return 0;
+    if(root.left == null && root.right == null){
+        return 1;
+    }
+
+    if(root.left==null)
+        return minDepth(root.right) + 1;
+    if(root.right==null)
+        return minDepth(root.left) + 1;
+
+    return Math.min(minDepth(root.left),minDepth(root.right)) + 1;
+}
