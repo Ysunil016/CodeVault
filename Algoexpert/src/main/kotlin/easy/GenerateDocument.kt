6f56532880852bab34b9d7151b9ae8b190d7767e
Sunil Yadav <sunil.yadav@thoughtworks.com>
2021-05-19 08:35:31 +0530

    sy generate document

--- Algoexpert/src/main/kotlin/easy/GenerateDocument.kt
+++ Algoexpert/src/main/kotlin/easy/GenerateDocument.kt
@@ -1,4 +1,31 @@
 package easy
 
 class GenerateDocument {
+
+    fun generateDocument(characters: String, document: String): Boolean {
+        val charStoreMap = mutableMapOf<Char, Int>()
+        characters.toCharArray().map {
+            if (charStoreMap.containsKey(it)) {
+                charStoreMap[it] = charStoreMap[it]!! + 1
+            } else {
+                charStoreMap[it] = 1
+            }
+        }
+
+        val documentStoreMap = mutableMapOf<Char, Int>()
+        document.toCharArray().map {
+            if (documentStoreMap.containsKey(it)) {
+                documentStoreMap[it] = documentStoreMap[it]!! + 1
+            } else {
+                documentStoreMap[it] = 1
+            }
+        }
+
+        document.forEach {
+            if (!charStoreMap.containsKey(it)) return false;
+            else if (documentStoreMap[it]!! > charStoreMap[it]!!) return false;
+        }
+        return true
+    }
+
 }
--- Algoexpert/src/test/kotlin/easy/GenerateDocumentTest.kt
+++ Algoexpert/src/test/kotlin/easy/GenerateDocumentTest.kt
@@ -1,5 +1,23 @@
 package easy
 
-import org.junit.jupiter.api.Assertions.*
+import org.junit.Assert
+import org.junit.Test
 
-internal class GenerateDocumentTest
+
+class GenerateDocumentTest{
+
+    @Test
+    fun generateDocument(){
+        val actualValue = GenerateDocument()
+            .generateDocument("Bste!hetsi ogEAxpelrt x ","AlgoExpert is the Best!")
+        Assert.assertTrue(actualValue)
+    }
+
+    @Test
+    fun generateDocument2(){
+        val actualValue = GenerateDocument()
+            .generateDocument("aheaollabbhb","hello")
+        Assert.assertTrue(actualValue)
+    }
+
+}
