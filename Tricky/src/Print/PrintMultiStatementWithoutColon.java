ecde776e07af60880e59adb4c9f7bbec95e69e4e
Sunil Yadav <sunil.yadav@thoughtworks.com>
2021-05-19 20:28:22 +0530

    sy update package and make module

--- Tricky/src/Print/PrintMultiStatementWithoutColon.java
+++
@@ -1,9 +0,0 @@
-package Print;
-
-public class PrintMultiStatementWithoutColon {
-
-	public static void main(String[] args) {
-		if (System.out.printf("Sunil Yadav") == null  || System.out.printf(" is Busy ") == null || System.out.printf("In Minting") == null) {
-		}
-	}
-}
