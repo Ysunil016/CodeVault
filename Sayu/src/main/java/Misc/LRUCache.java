ee813784714e88833523ed2852d52b292215ec03
Yadav, Sunil <sunil016@yahoo.com>
2023-04-08 01:55:44 +0530

    #Misc - update lru code

--- Sayu/src/main/java/Misc/LRUCache.java
+++ Sayu/src/main/java/Misc/LRUCache.java
@@ -4,7 +4,7 @@ import java.util.*;
 
 class LRUCache {
     public static void main(String[] args) {
-        Cache lru = new Cache(2);
+        Cache lru = new Cache(3);
 //
 //        SET 1 2 SET 2 3 SET 1 5
 //        SET 4 5 SET 6 7 GET 4 SET 1 2 GET 3
@@ -34,13 +34,56 @@ class LRUCache {
 //        System.out.println(lru.keyValue);
 
 //        [[2],[2,1],[1,1],[2,3],[4,1],[1],[2]]
-        lru.put(2, 1);
-        lru.put(1, 1);
-        lru.put(2, 3);
-        lru.put(4, 1);
-        System.out.println(lru.get(1));
-        System.out.println(lru.get(2));
 
+//        lru.put(2, 1);
+//        lru.put(1, 1);
+//        lru.put(2, 3);
+//        lru.put(4, 1);
+//        System.out.println(lru.get(1));
+//        System.out.println(lru.get(2));
+
+// [[3],[1,1],[2,2],[3,3],[4,4],[4],[3],[2],[1],[5,5],[1],[2],[3],[4],[5]]
+        lru.put(1, 1);
+        lru.head.display();
+
+        lru.put(2, 2);
+        lru.head.display();
+
+        lru.put(3, 3);
+        lru.head.display();
+
+        lru.put(4, 4);
+        lru.head.display();
+
+        System.out.println(lru.get(4));
+        lru.head.display();
+
+        System.out.println(lru.get(3));
+        lru.head.display();
+
+        System.out.println(lru.get(2));
+        lru.head.display();
+
+        System.out.println(lru.get(1));
+        lru.head.display();
+
+        lru.put(5, 5);
+        lru.head.display();
+
+        System.out.println(lru.get(1));
+        lru.head.display();
+
+        System.out.println(lru.get(2));
+        lru.head.display();
+
+        System.out.println(lru.get(3));
+        lru.head.display();
+
+        System.out.println(lru.get(4));
+        lru.head.display();
+
+        System.out.println(lru.get(5));
+        lru.head.display();
 
     }
 
@@ -68,11 +111,18 @@ class LRUCache {
             Node keyNode = keyValue.get(key);
             if (keyNode.next == null) return; // Tail Element
 
-            if (keyNode.prev == null) head = keyNode.next; // Head Element
+            if (keyNode.prev == null)
+                head = keyNode.next; // Head Element
+            else
+                keyNode.prev.next = keyNode.next; // Middle Element
 
-            // Middle Element
-            keyNode.prev = keyNode.next;
+            head.prev = null;
+
             tail.next = keyNode;
+            keyNode.prev = tail;
+            keyNode.next = null;
+
+            tail = tail.next;
         }
 
         private void updateNodeSequence(Node keyNode) {
@@ -82,6 +132,9 @@ class LRUCache {
                 return;
             }
             tail.next = keyNode;
+            keyNode.prev = tail;
+            keyNode.next = null;
+
             tail = tail.next;
         }
 
@@ -113,6 +166,12 @@ class LRUCache {
             Node(int key, int value) {
                 this.key = key;
                 this.value = value;
+            }
+
+            public void display() {
+                System.out.print(this.value + " ");
+                if (this.next != null) this.next.display();
+                else System.out.println();
             }
 
             @Override
